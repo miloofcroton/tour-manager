a00fc46d320bde75874fdd48fe82faaa77a28a36
Jack <ajackofsometrades@gmail.com>
2018-10-10 17:58:46 -0700

    passing all model validation tests

--- test/unit/tour.test.js
+++ test/unit/tour.test.js
@@ -1,21 +1,44 @@
-require('dotenv').config();
+const { getErrors } = require('./helpers');
 const Tour = require('../../lib/models/Tour');
-require('../../lib/utils/connect')();
+const Chance = require('chance');
+const chance = new Chance();
 
 describe('tour model', () => {
 
-    it('creates a new tour in my db', () => {
-        return Tour.create({
-            title: 'Ringling Bros',
+    it('validates a good model', () => {
+        const data = {
+            title: chance.string(),
+            activities: [chance.animal(), chance.animal()],
+            launchDate: chance.date(),
+            stops: [{
+                location: {
+                    city: chance.city(),
+                    state: chance.state(),
+                    zip: chance.zip()
+                },
+                weather: {
+                    temperature: chance.string(),
+                    condition: chance.string(),
+                    windSpeed: chance.string()
+                },
+                attendance: chance.natural({ min: 1 })
+            }]        
+        };
+        
+        const tour = new Tour(data);
+        const jsonTour = tour.toJSON();
+        expect(jsonTour).toEqual({ ...data, stops: expect.any(Array), _id: expect.any(Object) });
+        expect(jsonTour.stops[0]).toEqual({ ...data.stops[0], _id: expect.any(Object) });
+        expect(jsonTour.stops).toHaveLength(1);
+    });
+
+    it('title is required', () => {
+        const tour = new Tour({
             activities: ['Ringling', 'Being bros'],
             launchDate: Date.now()
-        })
-            .then(createdTour => {
-                expect(createdTour).toHaveProperty('_id');
-                expect(createdTour.title).toEqual('Ringling Bros');
-                expect(createdTour.toJSON().activities).toEqual(['Ringling', 'Being bros']);
-                expect(createdTour.launchDate).toBeInstanceOf(Date);
-            });
+        });
+        const errors = getErrors(tour.validateSync(), 1);
+        expect(errors.title.properties.message).toEqual('Path `title` is required.');
     });
 
 
