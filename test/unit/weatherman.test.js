60c38611a66bbccab00531a5e9b5acc2d9c76445
Jack <ajackofsometrades@gmail.com>
2018-10-11 11:48:29 -0700

    weatherman is working

--- test/unit/weatherman.test.js
+++ test/unit/weatherman.test.js
@@ -1,31 +1,27 @@
+require('dotenv').config();
 const weatherman = require('../../lib/util/weatherman');
 
 describe('the location/weather setter', () => {
 
-    it('gets the necessary info for a given zip code', () => {
+    it('gets the necessary info for a given zip code', done => {
 
-        const zip = 97220;
-        const example = {
-            weather:
-            {
-                temperature: '49.3',
-                condition: 'Clear',
-                windSpeed: '1',
-                windDir: 'NNE',
-                sunrise: '7:21',
-                sunset: '18:31'
-            },
-            location:
-            {
-                city: 'Portland',
-                state: 'OR',
-                country: 'US',
-                elevation: '61.9'
-            }
+        const req = {
+            body: { zip: '97209' }
         };
+        let error, called = false;
 
-        return weatherman(zip)
-            .then(res => console.log(res));
+        const next = err => {
+            called = true;
+            error = err;
+
+            expect(called).toBeTruthy();
+            expect(error).toBeUndefined();
+            expect(req.location).toEqual(expect.any(Object));
+            expect(req.weather).toEqual(expect.any(Object));
+            done();
+        };
+        weatherman()(req, null, next);
+
     });
 
 
